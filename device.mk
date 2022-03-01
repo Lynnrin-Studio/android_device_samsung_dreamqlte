90932e8c25c0c72069a8327d9e0359cb0f748929
LynnrinChan <wuxiaofei31@gmail.com>
2022-03-01 12:31:01 +0800

    dreamqlte: Drop Xiaomi Doze

--- device.mk
+++ device.mk
@@ -153,10 +153,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl
 
-# Doze
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
